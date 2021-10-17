dd2833228793aea26c7e6764bb9b8cccf876a65e
Wong Shouhao <42537782+wangshouh@users.noreply.github.com>
2021-10-17 16:27:24 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,89 @@
+import requests
+import json
+import time
+
+
+def getQuestionsId(uuid, courseId, recruitId) -> list:
+    """
+    获取前50条问题
+    """
+    questionid_list = []
+    questionUrl = "https://creditqa.zhihuishu.com/creditqa/web/qa/getHotQuestionList"
+    headers = {"Host": "creditqa.zhihuishu.com"}
+    params = {
+        "uuid": uuid,
+        "courseId": courseId,
+        "pageIndex": "0",
+        "pageSize": "50",
+        "recruitId": recruitId
+    }
+    questionData = requests.get(questionUrl, params=params, headers=headers)
+    r = json.loads(questionData.text)
+    question_list = r["rt"]["questionInfoList"]
+
+    for item in question_list:
+        questionid_list.append(item["questionId"])
+    return questionid_list
+
+
+def getAnswer(uuid, courseId, recruitId, questionId) -> str:
+    """
+    获取最近一条答案
+    """
+    answerUrl = "https://creditqa.zhihuishu.com/creditqa/web/qa/getAnswerInInfoOrderByTime"
+    headers = {"Host": "creditqa.zhihuishu.com"}
+    params = {
+        "uuid": uuid,
+        "questionId": questionId,
+        "courseId": courseId,
+        "recruitId": recruitId,
+        "sourceType": "2",
+        "pageIndex": "0",
+        "pageSize": "1"
+    }
+    answerData = requests.get(answerUrl, params=params, headers=headers)
+    r = json.loads(answerData.text)
+    answerContent = r["rt"]["answerInfos"][0]["answerContent"]
+    return answerContent
+
+
+def postAnswer(uuid, qid, answer, courseId, recruitId, cookies):
+    '''
+    发送答案
+    '''
+    url = "https://creditqa.zhihuishu.com/creditqa/web/qa/saveAnswer"
+    data = {
+        "uuid": uuid,
+        "qid": qid,
+        "annexs": "[]",
+        "source": "2",
+        "aContent": answer,
+        "courseId": courseId,
+        "recruitId": recruitId
+    }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Host": "creditqa.zhihuishu.com",
+        "Content-Length": "140"
+    }
+
+    returnmsg = requests.post(url, data=data, headers=headers, cookies=cookies)
+    r = json.loads(returnmsg.text)
+    msg = r["msg"]
+    return msg
+
+
+uuid = ""
+courseId = ""
+recruitId = ""
+cookies = {
+    
+}
+
+id_list = getQuestionsId(uuid, courseId, recruitId)
+for questionId in id_list:
+    print(questionId)
+    answer = getAnswer(uuid, courseId, recruitId, questionId)
+    result = postAnswer(uuid, questionId, answer,courseId, recruitId, cookies)
+    print(result)
+    time.sleep(3)
